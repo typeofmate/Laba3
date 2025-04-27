b7bd8f1c2d5e16c623e88f7db335201eb06c21a0
typeofmate <matwaypavlov@yandex.ru>
2025-04-27 20:09:31 +0300

    Shared_ptr added with counter

---
+++ src/shared_ptr.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <memory>
+
+int main() 
+{
+    auto cat = std::make_shared<std::string>("Гарик");
+    {
+        auto catCopy = cat; // Счетчик: 2
+        std::cout << *catCopy << " и " << *cat << std::endl;
+    } // 1
+    std::cout << *cat << " остался" << std::endl;
+    return 0; // 0 получается объект удален
+}
